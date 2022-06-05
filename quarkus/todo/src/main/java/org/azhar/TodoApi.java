bb4dab5cc2f622531784cb9a5cab8e074e3e0073
azhar337 <muhdazharali98@gmail.com>
2022-06-06 00:52:13 +0800

    Quarkus: oauth2 usage testing: failure

--- quarkus/todo/src/main/java/org/azhar/TodoApi.java
+++ quarkus/todo/src/main/java/org/azhar/TodoApi.java
@@ -12,10 +12,8 @@ import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 import javax.transaction.Transactional;
 import javax.ws.rs.*;
-import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-import javax.ws.rs.core.SecurityContext;
 
 @SecurityScheme(
         scheme = "bearer",
