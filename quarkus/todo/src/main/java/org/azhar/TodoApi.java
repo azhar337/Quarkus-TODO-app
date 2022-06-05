2aa6681e87b737e252faa4bf80006a1be8049062
azhar337 <muhdazharali98@gmail.com>
2022-06-06 00:06:39 +0800

    Quarkus: Solution 1 jwt bearer

--- quarkus/todo/src/main/java/org/azhar/TodoApi.java
+++ quarkus/todo/src/main/java/org/azhar/TodoApi.java
@@ -3,14 +3,25 @@ package org.azhar;
 
 import org.azhar.database.DatabaseRepository;
 import org.azhar.database.DatabaseResources;
+import org.eclipse.microprofile.openapi.annotations.enums.SecuritySchemeType;
+import org.eclipse.microprofile.openapi.annotations.security.SecurityScheme;
 
+import javax.annotation.security.PermitAll;
+import javax.annotation.security.RolesAllowed;
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 import javax.transaction.Transactional;
 import javax.ws.rs.*;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.SecurityContext;
 
+@SecurityScheme(
+        scheme = "bearer",
+        type = SecuritySchemeType.HTTP,
+        bearerFormat = "JWT"
+)
 @Path("/todo")
 @ApplicationScoped
 public class TodoApi {
@@ -18,8 +29,10 @@ public class TodoApi {
     @Inject
     DatabaseResources databaseResources;
 
+
     @PUT
     @Path("/add")
+    @RolesAllowed("ROLE_USER")
     @Transactional
     @Produces(MediaType.APPLICATION_JSON)
     @Consumes(MediaType.APPLICATION_JSON)
@@ -33,6 +46,7 @@ public class TodoApi {
 
     @GET
     @Path("/list")
+    @RolesAllowed("ROLE_USER")
     @Transactional
     @Produces(MediaType.APPLICATION_JSON)
     @Consumes(MediaType.APPLICATION_JSON)
@@ -46,6 +60,7 @@ public class TodoApi {
 
     @PUT
     @Path("/update")
+    @RolesAllowed("ROLE_USER")
     @Transactional
     @Produces(MediaType.APPLICATION_JSON)
     @Consumes(MediaType.APPLICATION_JSON)
@@ -59,6 +74,7 @@ public class TodoApi {
 
     @DELETE
     @Path("/delete")
+    @RolesAllowed("ROLE_USER")
     @Transactional
     @Produces(MediaType.APPLICATION_JSON)
     @Consumes(MediaType.APPLICATION_JSON)
